e5eb66a182c76550518cb78622915cb51cebc84f
Ethan Watson <ethanswatson@aggiemail.usu.edu>
2019-07-21 00:37:03 -0700

    started styling with react-bootstrap

--- src/components/applied/Page.js
+++ src/components/applied/Page.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Head from 'next/head';
+import Container from 'react-bootstrap/Container';
 import styled from '@emotion/styled';
 import colors from '../../constants/colors';
 import GlobalHeader from './GlobalHeader';
@@ -14,8 +16,16 @@ const StyledPage = styled.main`
 const Page = ({ children }) => {
   return (
     <StyledPage>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <GlobalHeader />
-      {children}
+      <Container>{children}</Container>
     </StyledPage>
   );
 };
