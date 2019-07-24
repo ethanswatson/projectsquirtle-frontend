7aaa0f5d92bc658eb2eff690ad93a8a5f0eb9f12
Ethan Watson <ethanswatson@aggiemail.usu.edu>
2019-07-24 00:01:30 -0700

    working on form submit handling

--- src/components/applied/JoinSessionForm.js
+++ src/components/applied/JoinSessionForm.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Formik } from 'formik';
 
 const JoinSessionForm = () => {
   return (
     <>
       <h1>Join a Quiz</h1>
-      <Form>
-        <Form.Control type='text' size='lg' placeholder='Session Id' />
-        <Button variant='primary' size='lg' type='submit' block>
-          Join
-        </Button>
-      </Form>
+      <Formik
+        initialValues={{ sessionId: null }}
+        validate={values => {
+          const errors = {};
+          if (!values.sessionId) {
+            errors.sessionId = 'Required';
+          }
+          return errors;
+        }}
+      >
+        <Form>
+          <Form.Control
+            type='text'
+            name='sessionId'
+            size='lg'
+            placeholder='Session Id'
+          />
+          <Button variant='primary' size='lg' type='submit' block>
+            Join
+          </Button>
+        </Form>
+      </Formik>
     </>
   );
 };
